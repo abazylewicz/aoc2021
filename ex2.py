e767768eb5a83e012ccd79ca594276da523ea494
a.bazylewicz <arkadiusz.bazylewicz@adform.com>
2021-12-02 15:04:44 +0100

    ex2 with class

--- ex2.py
+++ ex2.py
@@ -1,20 +1,30 @@
-distance = 0
-aim = 0
-depth = 0
+from dataclasses import dataclass
+
+
+@dataclass
+class Position:
+    distance: int
+    aim: int
+    depth: int
+
+    def move(self, move_value: int):
+        self.distance += move_value
+        self.depth += self.aim * move_value
+
+
+pos = Position(0, 0, 0)
 oldMeasurement = None
 with open('ex2.input') as inputFile:
     for line in inputFile:
         direction, value = line.strip().split(' ')
-        value = int(value)
+        value: int = int(value)
         if direction == 'down':
-            aim += value
+            pos.aim += value
         elif direction == 'up':
-            aim -= value
+            pos.aim -= value
         elif direction == 'forward':
-            distance += value
-            depth += aim * value
+            pos.move(value)
         else:
             raise RuntimeError(f"Unexpected direction {direction}")
 
-
-print(distance * depth)
+print(pos.distance * pos.depth)
